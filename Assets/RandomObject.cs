d133bb29dcfb33e2a28fbff5af519f01cf24ffb3
cor <cor@pruijs.nl>
2018-06-09 22:36:06 +0200

    Add random spawning items

---
+++ Assets/RandomObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomObject : MonoBehaviour {
+
+	public Sprite[] sprites;
+	// Use this for initialization
+	void Start () {
+		SpriteRenderer sr = GetComponent<SpriteRenderer>();
+		sr.sprite = sprites[Random.Range(0,sprites.Length)];
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/RandomObjectSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomObjectSpawner : MonoBehaviour {
+
+	public GameObject randomObject;
+	public float spawnInterval;
+	public float lastSpawn;
+	public float moveSpeed;
+	public float moveDistance;
+
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > lastSpawn + spawnInterval) {
+			GameObject randObj = (GameObject) Instantiate(randomObject, transform.position, Quaternion.identity);
+			GameObject.Destroy(randObj, 10);
+
+			lastSpawn = Time.time;
+		}
+	
+		transform.position = new Vector3(Mathf.Sin(Time.time * moveSpeed) * moveDistance, 5, 0);
+	}
+}
